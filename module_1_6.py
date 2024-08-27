1f6b91104b8aa7989f3b85ef3bd874316e7ef767
Sergey446128 <s.milyaev2018@yandex.ru>
2024-08-27 19:25:31 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,17 +1,17 @@
-# my_dict = {'Denis': 1975, 'Anton':1981, 'Lena': 1990}
-# print(my_dict)
-# print(my_dict['Denis'])
-# print(my_dict.get('Stas'))
-# my_dict.update({'Aiex':1995,'Sahsa':2001})
-# print(my_dict)
-# my_dict.pop('Lena')
-# print(my_dict)
-# print(my_dict.get('Lena'))
-# print(my_dict)
+my_dict = {'Denis': 1975, 'Anton':1981, 'Lena': 1990}
+print(my_dict)
+print(my_dict['Denis'])
+print(my_dict.get('Stas'))
+my_dict.update({'Aiex':1995,'Sahsa':2001})
+print(my_dict)
+a = my_dict.pop('Lena')
+print(a)
+print(my_dict)
 
-my_set = {1, 2, 3, 4, 5, 1, 2, 3, 4}
-print(my_set)
-my_set = {1, 2, 3, 4, 5, 1, 2, 3, 4, 'String', True,(1, 2, 3)}
-print(my_set)
-print(my_set.discard(2))
-print(my_set)
+
+# my_set = {1, 2, 3, 4, 5, 1, 2, 3, 4}
+# print(my_set)
+# my_set = {1, 2, 3, 4, 5, 1, 2, 3, 4, 'String', True,(1, 2, 3)}
+# print(my_set)
+# print(my_set.discard(2))
+# print(my_set)
